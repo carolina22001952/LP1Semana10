205e2d840e1e19141dca7002147fb61b7464d6f0
Carolina Bastos <carol1999b@gmail.com>
2023-05-19 10:40:12 +0100

    Creat StringCollections

---
+++ StringCollections/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StringCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+            Stack<string> stack = new Stack<string>();
+            Queue<string> queue = new Queue<string>();
+            HashSet<string> hashSet = new HashSet<string>();
+
+            string[] myString = {"hello" , "world", "ola", "world", "mundo"};
+
+            IEnumerable<string>[] collections = {list, stack, queue, hashSet };
+        
+            foreach(string s in myString)
+            {
+                list.Add(s);
+                stack.Push(s);
+                queue.Enqueue(s);
+                hashSet.Add(s);
+            }
+
+            foreach (IEnumerable<string> collection in collections)
+            {
+                Console.WriteLine(collection.GetType().Name);
+                foreach(string s in collection)
+                {
+                    Console.WriteLine($"\n{s}");
+                }
+            }
+
+        }
+    }
+}
