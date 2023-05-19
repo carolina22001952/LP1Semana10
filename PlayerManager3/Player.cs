85b2e36200a50bb13a369be302c50aa4975baf54
Carolina Bastos <carol1999b@gmail.com>
2023-05-19 10:22:45 +0100

    Creat PlayerManager3

---
+++ PlayerManager3/Player.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlayerManager3 
+{
+    public class Player : IComparable<Player>
+    {
+        public string Name { get; }
+        public int Score { get; }
+
+        public Player(string name, int score)
+        {
+            Name = name;
+            Score = score;
+        }
+
+        public int CompareTo(Player other)
+        {
+            if(other is null) return 1;
+            return other.Score - Score;
+        }
+    }
+}
